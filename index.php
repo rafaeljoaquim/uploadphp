92add5434c6e4c3683821187b33cb96434abd9e0
rafaeljoaquim <skytek58@hotmail.com>
2016-08-04 11:08:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <form method="post" action="recebe_upload.php" enctype="multipart/form-data">
   <label>Arquivo</label>
+  /*onde o usuario ecolhera a imagem*/
   <input type="file" name="arquivo" />
   
-  <input type="submit" value="Enviar" />
+  <input type="submit" value="Fazer upload" />
 </form>
